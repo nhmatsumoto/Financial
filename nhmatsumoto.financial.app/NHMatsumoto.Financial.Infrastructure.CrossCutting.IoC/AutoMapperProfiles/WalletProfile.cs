93cbc686c6212899258eb5c822d76b557aa4cba8
Nilton Hiroyuki Matsumoto <hiroyukims@hotmail.com>
2024-01-07 07:07:17 -0300

    Organizando referencias assembly

--- nhmatsumoto.financial.app/NHMatsumoto.Financial.Infrastructure.CrossCutting.IoC/AutoMapperProfiles/WalletProfile.cs
+++ nhmatsumoto.financial.app/NHMatsumoto.Financial.Infrastructure.CrossCutting.IoC/AutoMapperProfiles/WalletProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using nhmatsumoto.financial.infrastructure.database.Tables;
+using NHMatsumoto.Financial.Application.DTOs.Inputs;
 
 namespace NHMatsumoto.Financial.Infrastructure.CrossCutting.IoC.Profiles
 {
@@ -8,7 +9,6 @@ namespace NHMatsumoto.Financial.Infrastructure.CrossCutting.IoC.Profiles
         public WalletProfile()
         {
             CreateMap<WalletTable, CreateWallet>().ReverseMap();
-            
         }
     }
 }
